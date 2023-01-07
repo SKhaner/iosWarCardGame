f6c6b25ef43e7b22ac80e7e2605c85bafbb1a247
Seeam Khan <seeam.khan2003@gmail.com>
2023-01-07 18:11:47 -0500

    Add files via upload

---
+++ war-challange/war-challange/war_challangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challangeApp.swift
+//  war-challange
+//
+//  Created by Seeam Khan on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
